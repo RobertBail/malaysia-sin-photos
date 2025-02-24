cb40e359733c5740116d3baaa94c9fbf1336534d
Robert Bailey <r.bail@hotmail.com>
2025-02-25 00:07:33 +1030

    photos

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <header>
+      <h1>Rob B's Malaysia/Singapore Photos Feb 2025</h1>
+      <h4>In February 2025, I went to Malaysia and Singapore for a week or so. These are mainly photos of Malaysia, some places/things I hadn't been to before in Malaysia, or since the January 2001 trip when I was 12. It was mainly a guy called Amir and his Chinese friend (I forget his name because I hadn't slept much for 2 or 3 days) who drove me over from Singapore and showed me around. Amir is/was aged 70 (?), retired from Malaysian Air Force, mainly as an aircraft mechanic he said, on C130 Hercules. He said in 1978 he was in RAAF Wagga Wagga, as part of an exercise or allied exchange/cooperation thing. The Chinese friend was aged 40 (?) and didn't speak as much English, but was pretty nice and showed me some of the temples, eg. the Chinese temple.</h4>
+      </header>
+      
+      <div>
+  <ul>
+    <li>
+      <img src="/001.jpg" alt="Image 1" />
+      <p>While at this mosque in Putrajaya, Amir introduced me to a woman, another guide/instructor, who discussed with me a few things about the architecture of the mosque, such as apparently small vents in the dome/ceiling of the mosque to let out hot air, an earlier form of air conditioning/ventilation I gather.</p>
+      <img src="/002.jpg" alt="Image 2" />
+      <img src="/003.jpg" alt="Image 3" />
+      <img src="/004.jpg" alt="Image 4" />
+      <img src="/005.jpg" alt="Image 5" />
+      <img src="/036.jpg" alt="Image 36" />
+      <img src="/037.jpg" alt="Image 37" />
+      <img src="/015.jpg" alt="Image 15" />
+      <p>Batu Caves, with the steps up to the caves, which I/we went up.</p>
+      <img src="/053.jpg" alt="Image 53" />
+      <img src="/047.jpg" alt="Image 47" />
+      <img src="/017.jpg" alt="Image 17" />
+      <img src="/042.jpg" alt="Image 42" />
+      <img src="/009.jpg" alt="Image 9" />
+      <p>A few of the monkeys (towards left) in/around the Batu Caves.</p>
+      <img src="/019.jpg" alt="Image 19" />
+      <img src="/056.jpg" alt="Image 56" />
+      <img src="/045.jpg" alt="Image 45" />
+      <img src="/020.jpg" alt="Image 20" />
+      <img src="/043.jpg" alt="Image 43" />
+      <img src="/059.jpg" alt="Image 59" />
+      <p>Amir took me to lunch at this revolving restaurant, a buffet, up the KL Tower.</p>     
+      <img src="/060.jpg" alt="Image 61" />
+      <img src="/063.jpg" alt="Image 63" />
+      <img src="/067.jpg" alt="Image 67" />
+      <p>A Royal Malaysian guard/government site. As Amir noted, the combination of British tradition, like at Buckingham Palace with the guards standing still and on horses, and Malay/Muslim tradition. </p>
+      <img src="/069.jpg" alt="Image 69" />
+      <p>The Chinese Temple</p>
+      <img src="/070.jpg" alt="Image 70" />
+      <img src="/071.jpg" alt="Image 71" />
+      <p>Image 1</p>
+      <p>Image 1</p>
+    </li>
+  </ul>
+</div>
+
+ 
+     </div>
+    </>
+  )
+}
+
+export default App
