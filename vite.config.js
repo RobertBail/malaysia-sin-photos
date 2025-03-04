3576041e275655d7103112b358bb9cd35c5cc606
Robert Bailey <r.bail@hotmail.com>
2025-03-04 20:42:15 +1030

    photos

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ClosePlugin from './vite-plugin-close.js'
 
 // https://vite.dev/config/
 export default defineConfig({
   
-  plugins: [react()],
+  plugins: [react(),ClosePlugin()],
+  build: {
+    sourcemap: false,  // Disable source maps in production
+  },
 
 })
