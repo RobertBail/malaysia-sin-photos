ffd78ba4f5d4b782c7163034531241f3e8f84326
Robert Bailey <r.bail@hotmail.com>
2025-03-04 17:00:27 +1030

    photos

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
      <div>
       <header>
       <h1>Rob B's Malaysia/Singapore Photos Feb 2025</h1>
-      <h4>In February 2025, I went to Malaysia and Singapore for a week or so. These are mainly photos of Malaysia, some places/things I hadn't been to before in Malaysia, or since the January 2001 trip when I was 12. It was mainly a guy called Amir and his Chinese friend (I forget his name because I hadn't slept much for 2 or 3 days) who drove me over from Singapore and showed me around. Amir is/was aged 70 (?), retired from Malaysian Air Force, mainly as an aircraft mechanic he said, on C130 Hercules. He said in 1978 he was in RAAF Wagga Wagga, as part of an exercise or allied exchange/cooperation thing. The Chinese friend was aged 40 (?) and didn't speak as much English, but was pretty nice and showed me some of the temples, eg. the Chinese temple.</h4>
+      <h4>In February 2025, I went to Malaysia and Singapore for a week or so. These are mainly photos of Malaysia, some places/things I hadn't been to before in Malaysia, or since the January 2001 trip when I was 12. It was mainly a guy called Amir and his Chinese friend (I forget his name because I hadn't slept much for 2 or 3 days) who drove me over from Singapore and showed me around. Amir is/was aged 70 (?), retired from Malaysian Air Force, mainly as an aircraft mechanic he said, on C130 Hercules. He said in 1978 he was in RAAF Wagga Wagga, as part of an exercise or allied exchange/cooperation thing. The Chinese friend was aged 40 (?) and was pretty nice and showed me some of the temples, eg. the Chinese temple.</h4>
       </header>
       
       <div>
@@ -44,16 +44,42 @@ function App() {
       <img src="/077.jpg" alt="Image 77" />
       <img src="/063.jpg" alt="Image 63" />
       <img src="/075.jpg" alt="Image 75" />
+      <img src="/165.jpg" alt="Image 165" />
       <img src="/067.jpg" alt="Image 67" />
       <p>A Royal Malaysian guard/government site. As Amir noted, the combination of British tradition, like at Buckingham Palace with the guards standing still and on horses, and Malay/Muslim tradition. </p>
       <img src="/073.jpg" alt="Image 73" />
       <img src="/069.jpg" alt="Image 69" />
       <p>The Chinese Temple</p>
       <img src="/070.jpg" alt="Image 70" />
-      <img src="/083.jpg" alt="Image 83" />
+      <img src="/125.jpg" alt="Image 125" />
       <img src="/087.jpg" alt="Image 87" />
-      <p>Image 1</p>
-      <p>Image 1</p>
+      <img src="/176.jpg" alt="Image 176" />
+      <img src="/115.jpg" alt="Image 115" />
+      <p>At the war memorial, with Amir.</p>
+      <img src="/117.jpg" alt="Image 117" />
+      <img src="/143.jpg" alt="Image 143" />
+      <img src="/152.jpg" alt="Image 152" />
+      <img src="/154.jpg" alt="Image 154" />
+      
+      <p>Some bits of Singapore:</p>
+      <img src="/188.jpg" alt="Image 188" />
+      <img src="/190.jpg" alt="Image 190" />
+      <img src="/192.jpg" alt="Image 192" />
+      <p>The boardwalk to/from Sentosa. I hadn't been over to Sentosa since January 2005, so interesting to see.</p>
+      <img src="/239.jpg" alt="Image 239" />
+      <p>Trying out a day room at Crowne Plaza Changi, with a view of aircraft taking off:</p>
+      <img src="/286.jpg" alt="Image 286" />
+      <img src="/290.jpg" alt="Image 290" />
+      <p>The tail appearing below/next to this A380 is/was of a Lufthansa 747:</p>
+      <img src="/317.jpg" alt="Image 317" />   
+      <img src="/326.jpg" alt="Image 326" />
+      <p>A few dodgy shots from trying out ScootPlus:</p>
+      <img src="/240.jpg" alt="Image 240" />
+      <p>I'd never been in Business class or Premium Economy before, so interesting to try.</p>
+      <img src="/269.jpg" alt="Image 269" />
+      <p>And the cockpit door was half open before the flight, so could see some of the instruments, displays and controls, including throttles, for this 787. The blue screen looked to be a map of Changi Airport, probably to assist with taxiing instead of the old black and white paper charts.</p>
+      <img src="/273.jpg" alt="Image 273" />
+      
     </li>
   </ul>
 </div>
